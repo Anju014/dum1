53f052bfbe3e43fd725760a3a45277e164187666
Anju014 <84642983+Anju014@users.noreply.github.com>
2023-02-17 19:02:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+
+source = "/Users/anju/Downloads/Coding/PYTHON/Basics/C102/daisy.png"
+destination = "/Users/anju/Downloads/Coding/PYTHON/Basics/C102/Pictures"
+
+shutil.copy(source, destination)
